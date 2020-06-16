ee62a2914ad1697d88a80a568426bdec34d820e8
sharon <sharon@ameritest.com>
2020-06-16 09:59:51 -0600

    Validations,new and create methods, flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,32 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-
   def show
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:notice] = "New article was successfully created!"
+      redirect_to @article
+    else
+      flash[:alert] = "Uh oh! The article was not created!"
+      render 'new'
+    end
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(
+        :title,
+        :description
+      )
+    end
 end
