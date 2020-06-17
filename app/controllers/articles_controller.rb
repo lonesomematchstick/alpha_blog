be439d8be9db07af70cb618106c669ac00a3d3a5
sharon <sharon@ameritest.com>
2020-06-16 19:05:32 -0600

    Add layout features and delete action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,12 +16,36 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      flash[:notice] = "New article was successfully created!"
+      flash[:notice] = "The article was successfully created!"
       redirect_to @article
     else
       flash[:alert] = "Uh oh! The article was not created!"
       render 'new'
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    @article.update(article_params)
+    if @article.save
+      flash[:notice] = "The article was successfully updated!"
+      redirect_to @article
+    else
+      flash[:alert] = "Uh oh! The article was not updated!"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:alert] = "The article was destroyed."
+    redirect_to articles_path
   end
 
   private
